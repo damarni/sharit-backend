4eefa06c7c17043629547592aca7a3537496c369
David Tamayo <David Tamayo>
2016-04-06 20:15:12 +0200

    pantalla inicial test

--- controllers/default.go
+++ controllers/default.go
@@ -13,9 +13,9 @@ type MainController struct {
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
-	//c.TplName = "index.tpl"
-	c.Data["json"] = "astaxie@gmail.com"
-	c.ServeJSON()
+	c.TplName = "index.tpl"
+	//c.Data["json"] = "astaxie@gmail.com"
+	//c.ServeJSON()
 
 	//c.ServeJSON()
 
