de697a139cede8ab32715da4906d8f9b3a335851
OLGA <olga.carbo@est.fib.upc.edu>
2016-04-13 12:04:01 +0200

    update amb coordenades

--- models/user.go
+++ models/user.go
@@ -54,7 +54,7 @@ func (u *User) UpdateUser() error {
 	db := mongo.Conn()
 	defer db.Close()
 	c := db.DB(beego.AppConfig.String("database")).C("users")
-	err := c.Update(bson.M{"iduser": u.IDuser}, bson.M{"$set": bson.M{"email": u.Email}})
+	err := c.Update(bson.M{"iduser": u.IDuser}, bson.M{"$set": bson.M{"email": u.Email, "x": u.X, "y": u.Y}})
 	return err
 }
 
